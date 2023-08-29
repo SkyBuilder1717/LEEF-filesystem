885eb5809f957edec70457c426f16714381c8dad
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2023-08-28 17:38:09 -0700

    added various depends for MTUL-b3d

---
+++ init.lua
@@ -0,0 +1,16 @@
+mtul = {
+    binary = {}, --provided entirely by modlib.
+    tbl = {} --modified by libs.modlib, source.
+}
+
+--optionally allow user to overset local math with mtul while still keeping core funcs & consts.
+mtul.math = {}
+for i, v in pairs(math) do
+    mtul[i] = v
+end
+
+--run files. These will directly modify the mtul sub tables.
+mtul.path = minetest.get_modpath("MTUL-core")
+dofile(mtul.path.."/libs/modlib/binary.lua")
+dofile(mtul.path.."/libs/modlib/table.lua")
+dofile(mtul.path.."/libs/modlib/math.lua")
