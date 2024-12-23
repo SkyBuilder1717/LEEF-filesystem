a10f02f857bb5a3382ed961c6ca19d5db8b44cf4
FatalErr42O <58855799+FatalError42O@users.noreply.github.com>
2024-12-22 16:11:05 -0800

    change some weird licensing

--- init.lua
+++ init.lua
@@ -8,6 +8,6 @@ leef.paths = leef.paths or {}
 leef.loaded_modules.filesystem = true
 --run files. These will directly modify the leef sub tables.
 local path = minetest.get_modpath("leef_filesystem")
-dofile(path.."/modlib/binary.lua")
-dofile(path.."/modlib/mod_utils.lua")
-dofile(path.."/modlib/paths.lua")
+dofile(path.."/binary.lua")
+dofile(path.."/mod_utils.lua")
+dofile(path.."/paths.lua")
